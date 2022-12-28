5deaf6f241614117d5a6e88ce172f63d93c45378
sabari vignesh <sabnagaraj@paypal.com>
2022-12-28 15:31:42 +0530

    done chqnages

--- routes.js
+++ routes.js
@@ -1,8 +1,8 @@
 var express = require ('express');
 var router = express.Router();
-var usercontroller = require ('./app/controllers/UserController')
 
 
+// asd
 
 
 router.post('/user/create',usercontroller.createUser)
